badc1f1bd72db0747cade758af590c8d0b9da975
Cheyenne Mero <cmero@va-d5.kzn.21-school.ru>
2021-06-07 13:19:33 +0300

    Get_Next_Line

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_sump_checker(char **sump, char **line);
+int		ft_checker_mallocer(int fd, char **line, char **buf);
+void	ft_connector_liberator(char **mem_liber, char **line, char *buf);
+char	*ft_new_line_checker(char *s);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+
+#endif
